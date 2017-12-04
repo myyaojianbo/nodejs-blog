bef835209fa82b47c823959b60c6ad8c49f77439
“myyaojianbo <1038749096@qq.com>
2017-12-04 20:42:56 +0800

    优化删除功能---异步

--- routes/index.js
+++ routes/index.js
@@ -45,37 +45,38 @@ router.get('/login', function(req, res, next) {
 router.post('/login', function(req, res, next) {
     //req.body 处理 post 请求
     var username = req.body.username,
-    password = req.body.password;
+        password = req.body.password;
         // console.log('user',username);
-        User.find({
-          username:username,
-          password:password
-        },function(err,newUser){
-          if(err){
-            console.log('err');
-          }
-          if(newUser !=""){
-            console.log(newUser);
-            console.log('登陆成功');
-            req.flash('success',"登录成功！");
-            req.session.user = newUser[0];
-            return res.redirect('/');
-          }else{
-            console.log('用户名户密码错误');
-            req.flash('error',"用户名户密码错误");
-            return res.redirect("/login");
-          }
-        });
-      });
+    User.find({
+        username:username,
+        password:password
+      },function(err,newUser){
+        if(err){
+          // console.log('err');
+          req.flash("error",error);
+        }
+        if(newUser !=""){
+          // console.log(newUser);
+          // console.log('登陆成功');
+          req.flash('success',"登录成功！");
+          req.session.user = newUser[0];
+          return res.redirect('/');
+        }else{
+          console.log('用户名户密码错误');
+          req.flash('error',"用户名户密码错误");
+          return res.redirect("/login");
+        }
+    });
+});
 
 
 /* 退出登录 */
 router.get('/logout', function(req, res, next) {
-  req.session.user =null;
+    req.session.user =null;
     // res.render('logout', {title: 'logout'});
     req.flash('success',"退出成功");
     return res.redirect('/');
-  });
+});
 
 
 /* 注册 */
@@ -84,52 +85,49 @@ router.get('/reg', function(req, res, next) {
     title: 'reg',
     success:req.flash('success').toString(),
     error:req.flash('error').toString()
-      // success:req.flash('success'),
-      // error:req.flash('error')
-    })
+  })
 });
 
 router.post('/reg', function(req, res, next) {
     //req.body 处理 post 请求
     var username = req.body.username,
-    password = req.body.password,
-    passwordRepeat = req.body.passwordRepeat,
-    email = req.body.email;
+        password = req.body.password,
+        passwordRepeat = req.body.passwordRepeat,
+        email = req.body.email;
 
       // console.log('user',username);
-      if(password != passwordRepeat) {
-        console.log('两次输入的密码不一致！');
-        req.flash('error',"两次输入内容不一致");
+    if(password != passwordRepeat) {
+      // console.log('两次输入的密码不一致！');
+      req.flash('error',"两次输入内容不一致");
+      return res.redirect('/reg');
+    }
+
+    User.findOne({username:username}, function(err, user) {
+      if(err) {
+        console.log(err);
         return res.redirect('/reg');
       }
 
-      User.findOne({username:username}, function(err, user) {
+      if(user) {
+        console.log('用户名已经存在');
+        req.flash('error',"用户名已存在");
+        return res.redirect('/reg'); 
+      }
+
+      // var md5 = crypto.createHash('md5'),
+      //     md5password = md5.update(password).digest('hex');
+
+      var newUser = new User({
+        username: username,
+        password: password,
+        email: email
+      });
+
+      newUser.save(function(err, doc) {
         if(err) {
           console.log(err);
           return res.redirect('/reg');
-        }
-
-        if(user) {
-          console.log('用户名已经存在');
-          req.flash('error',"用户名已存在");
-          return res.redirect('/reg');
-        }
-
-        // var md5 = crypto.createHash('md5'),
-        //     md5password = md5.update(password).digest('hex');
-
-        var newUser = new User({
-          username: username,
-          password: password,
-          email: email
-        });
-
-        newUser.save(function(err, doc) {
-          if(err) {
-            console.log(err);
-            return res.redirect('/reg');
-          }
-
+        }else{
           console.log('注册成功！');
           req.flash('success',"注册成功");
 
@@ -137,10 +135,11 @@ router.post('/reg', function(req, res, next) {
           delete newUser.password;
           req.session.user = newUser;
           return res.redirect('/');
-        });
-
+        }
       });
-    });
+
+  });
+});
 
 
 /* 发表文章 */
@@ -155,30 +154,30 @@ router.get('/post', function(req, res, next) {
 
 router.post('/post', function(req, res, next) {
   var data = new Article({
-            title: req.body.title,
-            tag: req.body.tag,
-             //这里的 author 元素通过 session 获得
-             author: req.session.user.username,
-             createTime:Date.now(),
-             content: req.body.content
-           });
+    title: req.body.title,
+    tag: req.body.tag,
+     //这里的 author 元素通过 session 获得
+    author: req.session.user.username,
+    createTime:Date.now(),
+    content: req.body.content
+  });
 
   data.save(function(err, doc) {
     if(err) {
       console.log(err);
       return res.redirect('/post');
-    }
-    console.log('文章发表成功！');
-    req.flash('success',"文章发表成功")
-    
-    return res.redirect('/');
+    }else{
+      // console.log('文章发表成功！');
+      req.flash('success',"文章发表成功");
+      return res.redirect('/');
+    } 
   });
-
 });
 
 
 /* 查询*/
 router.get('/search', function(req, res, next) {
+  //  想办法使用ajax 并且不用search页面 和index.ejs页面共用
   var arts = ""
   //获取输入的标题 
   var title = req.query.keyTitle;
@@ -194,10 +193,8 @@ router.get('/search', function(req, res, next) {
       res.render('search', {
         arts:articles,
         user:req.session.user
-        });
-
+      });
     })
-
 });
 
 
@@ -235,51 +232,41 @@ router.post('/edit/:_id', function(req, res, next) {
     if(err){
       console.log('123');
       console.log('err',err);
+    }else{
+      console.log('修改成功');
+      return res.redirect('/');
     }
-    
-    console.log('修改成功');
-    return res.redirect('/');
   });
 });
 
 /* 删除*/
-router.get('/remove/:_id', function(req, res, next) {
+router.get('/remove', function(req, res, next) {
   Article.remove({
-    //req.params可以处理 /:_id 形式的get或者post请求，获取请求参数
-    _id:req.params._id
-    },function(err){
+    _id:req.query.id
+  },function(err,art){
     if(err){
-      console.log(err);
+      console.log('error',err);
+    }else{
+      // console.log('成功了');
+      res.json({"rmit":1});
     }
-    console.log('删除成功');
-    return res.redirect('/');
   })
-});
+})
 
 /* 点赞*/
 router.get('/dianzan',function(req,res,next){
-   // console.log(req.query.id) 
-   // Article.findOne({_id:req.query.id},{},function(err,art){
-   //   console.log('art',art);
-   //   // if err   else
-   //   // if(err){
-   //   //  console.log('err',err);
-   //   // }else{
-   //   //    art._doc.dz=1;
-   //   //    console.log('art111111',art);
-   //   //    res.json({'sd':1});
-   //   // }
-   //    art._doc.dz=1;
-   //    console.log('art111111',art);
-   //    // res.json({'sd':1});
-   // })
  Article.update({
     _id:req.query.id
   },{
     dz:1
-  },function(err,art){   
-    console.log('art111111',art);
-    res.json({'sd':1});
+  },function(err,art){  
+    if(err){
+      console.log('error',err);
+    }else{
+      // console.log('art111111',art);
+      res.json({'sd':1});
+    }
+    
   });
 });
 
